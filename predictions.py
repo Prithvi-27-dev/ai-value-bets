a894e1a9b1423bd186724d9b5a22474386084182
Prithviraj Ramgati <prithvi.soccer27@gmail.com>
2025-08-09 11:29:48 +0400

    Delete predictions.py

--- predictions.py
+++
@@ -1,6 +0,0 @@
-def get_predictions():
-    return [
-        {"Match": "Liverpool vs Man City", "Market": "1X2", "Prediction": "Liverpool", "AI Confidence": "72"},
-        {"Match": "PSG vs Lyon", "Market": "Over/Under 2.5", "Prediction": "Over", "AI Confidence": "81"},
-        {"Match": "Real Madrid vs Barcelona", "Market": "BTTS", "Prediction": "Yes", "AI Confidence": "65"}
-    ]
